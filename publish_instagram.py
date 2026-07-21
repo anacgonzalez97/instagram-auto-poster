cf82009d5f17580dddd6c1f764515d9798fbc252
ana <anabel.cgonzalez@gmail.com>
2026-07-21 13:37:15 +0200

    Fix folder ID in pick_image function
    Removed unnecessary query parameters from folder ID.

--- publish_instagram.py
+++ publish_instagram.py
@@ -65,7 +65,7 @@ def get_drive_service():
 def pick_image(service):
     """Selecciona una imagen de la carpeta de origen."""
     # Reemplaza la cadena de abajo por el ID real de tu carpeta de Drive:
-    folder_id = "1cSK8eSFQ88nFdERpEDJ2gRa1asRv_Db5?dmr=1&ec=wgc-drive-%5Bmodule%5D-goto"
+    folder_id = "1cSK8eSFQ88nFdERpEDJ2gRa1asRv_Db5"
 
     query = (
         f"'{folder_id}' in parents "
